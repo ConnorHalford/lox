4f2f0317bbdc2e1be4b160230e50ae8e75d49bee
ConnorHalford <connor.halford@hotmail.co.uk>
2021-09-03 19:30:39 +0100

    AST generator now includes Visitor pattern

--- GenerateAST/Code/GenerateAST.cs
+++ GenerateAST/Code/GenerateAST.cs
@@ -15,10 +15,10 @@ public class GenerateAST
 
 		string outputDir = args[0];
 		DefineAST(outputDir, "Expr", new List<string>() {
-				"Binary		: Expr left, Token operation, Expr right",
-				"Grouping	: Expr expression",
-				"Literal	: object value",
-				"Unary		: Token operation, Expr right"
+				"Binary		: Expr Left, Token Operation, Expr Right",
+				"Grouping	: Expr Expression",
+				"Literal	: object Value",
+				"Unary		: Token Operation, Expr Right"
 			});
 
 		return 0;
@@ -35,24 +35,42 @@ public class GenerateAST
 			sw.WriteLine($"public abstract class {baseName}");
 			sw.WriteLine("{");
 
+			DefineVisitor(sw, baseName, types);
+
 			// The AST classes
 			int numTypes = types.Count;
 			for (int i = 0; i < numTypes; ++i)
 			{
-				if (i > 0)
-				{
-					sw.WriteLine();
-				}
-
 				string type = types[i];
 				string[] parts = type.Split(':', StringSplitOptions.TrimEntries);
 				string className = parts[0];
 				string fields = parts[1];
+				sw.WriteLine();
 				DefineType(sw, baseName, className, fields);
 			}
 
+			// The base Accept() method
+			sw.WriteLine();
+			sw.WriteLine("	public abstract T Accept<T>(Visitor<T> visitor);");
+
 			sw.WriteLine("}");
 		}
+	}
+
+	private static void DefineVisitor(StreamWriter sw, string baseName, List<string> types)
+	{
+		sw.WriteLine("	public interface Visitor<T>");
+		sw.WriteLine("	{");
+
+		string lowerBaseName = baseName.ToLower();
+		int numTypes = types.Count;
+		for (int i = 0; i < numTypes; ++i)
+		{
+			string typeName = types[i].Split(':', StringSplitOptions.TrimEntries)[0];
+			sw.WriteLine($"		T Visit{typeName}{baseName}({typeName} {lowerBaseName});");
+		}
+
+		sw.WriteLine("	}");
 	}
 
 	private static void DefineType(StreamWriter sw, string baseName, string className, string fieldList)
@@ -60,24 +78,58 @@ public class GenerateAST
 		sw.WriteLine($"	public class {className} : {baseName}");
 		sw.WriteLine("	{");
 
+		string[] fields = fieldList.Split(',', StringSplitOptions.TrimEntries);
+		int numFields = fields.Length;
+		string[] fieldTypes = new string[numFields];
+		string[] fieldNames = new string[numFields];
+		string[] fieldNamesLower = new string[numFields];
+		for (int i = 0; i < numFields; ++i)
+		{
+			string[] field = fields[i].Split(' ');
+			fieldTypes[i] = field[0];
+			fieldNames[i] = field[1];
+
+			// Convert first letter to lowercase if necessary
+			string lower = field[1];
+			if (!string.IsNullOrEmpty(lower) && lower[0] >= 'A' && lower[0] <= 'Z')
+			{
+				lower = (lower.Length == 1) ? lower.ToLower() : $"{lower.Substring(0, 1).ToLower()}{lower.Substring(1)}";
+			}
+			fieldNamesLower[i] = lower;
+		}
+
 		// Constructor
-		sw.WriteLine($"		public {className}({fieldList})");
+		sw.Write($"		public {className}(");
+		for (int i = 0; i < numFields; ++i)
+		{
+			if (i > 0)
+			{
+				sw.Write(", ");
+			}
+			sw.Write(fieldTypes[i]);
+			sw.Write(" ");
+			sw.Write(fieldNamesLower[i]);
+		}
+		sw.WriteLine(")");
 		sw.WriteLine("		{");
 
 		// Assign parameters to fields
-		string[] fields = fieldList.Split(',', StringSplitOptions.TrimEntries);
-		int numFields = fields.Length;
 		for (int i = 0; i < numFields; ++i)
 		{
-			string field = fields[i];
-			string name = field.Split(' ')[1];
-			sw.WriteLine($"			this.{name} = {name};");
+			sw.WriteLine($"			this.{fieldNames[i]} = {fieldNamesLower[i]};");
 		}
 
 		sw.WriteLine("		}");
+
+		// Visitor pattern
 		sw.WriteLine();
+		sw.WriteLine("		public override T Accept<T>(Visitor<T> visitor)");
+		sw.WriteLine("		{");
+		sw.WriteLine($"			return visitor.Visit{className}{baseName}(this);");
+		sw.WriteLine("		}");
 
 		// Define fields
+		sw.WriteLine();
 		for (int i = 0; i < numFields; ++i)
 		{
 			sw.WriteLine($"		public {fields[i]};");

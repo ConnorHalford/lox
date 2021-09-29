78be37d87ede437bb6ef32ac3396724ba6fed1ee
ConnorHalford <connor.halford@hotmail.co.uk>
2021-09-29 22:36:23 +0100

    String values, concatenation and freeing

--- BytecodeVM/Lox/memory.c
+++ BytecodeVM/Lox/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "vm.h"
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize)
 {
@@ -16,4 +17,29 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize)
 		exit(1);
 	}
 	return result;
+}
+
+static void freeObject(Obj* obj)
+{
+	switch (obj->type)
+	{
+		case OBJ_STRING:
+			{
+				ObjString* string = (ObjString*)obj;
+				FREE_ARRAY(char, string->chars, string->length + 1);
+				FREE(ObjString, obj);
+				break;
+			}
+	}
+}
+
+void freeObjects()
+{
+	Obj* obj = vm.objects;
+	while (obj != NULL)
+	{
+		Obj* next = obj->next;
+		freeObject(obj);
+		obj = next;
+	}
 }

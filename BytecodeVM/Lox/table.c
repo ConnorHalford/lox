1c99309ac1dcfe1c3da88aae8251bf9db9967d57
ConnorHalford <connor.halford@hotmail.co.uk>
2021-11-03 21:55:11 +0000

    Optimized hash table probing by replacing modulo with bitwise AND of (2^n)-1

--- BytecodeVM/Lox/table.c
+++ BytecodeVM/Lox/table.c
@@ -23,7 +23,7 @@ void freeTable(Table* table)
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
 {
-	uint32_t index = key->hash % capacity;
+	uint32_t index = key->hash & (capacity - 1);
 	Entry* tombstone = NULL;
 	for (;;)
 	{
@@ -49,7 +49,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key)
 			// We found the key
 			return entry;
 		}
-		index = (index + 1) % capacity;
+		index = (index + 1) & (capacity - 1);
 	}
 }
 
@@ -156,7 +156,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
 		return NULL;
 	}
 
-	uint32_t index = hash % table->capacity;
+	uint32_t index = hash & (table->capacity - 1);
 	for (;;)
 	{
 		Entry* entry = &table->entries[index];
@@ -176,7 +176,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
 			return entry->key;
 		}
 
-		index = (index + 1) % table->capacity;
+		index = (index + 1) & (table->capacity - 1);
 	}
 }
 

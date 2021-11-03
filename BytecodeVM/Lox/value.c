4eee82ba34abb9e6ed05c5768c13b218d3a6dfd9
ConnorHalford <connor.halford@hotmail.co.uk>
2021-11-03 22:37:00 +0000

    Optimized Value memory representation by using NaN Boxing to reduce type size from 16 bytes to 8 bytes

--- BytecodeVM/Lox/value.c
+++ BytecodeVM/Lox/value.c
@@ -7,6 +7,13 @@
 
 bool valuesEqual(Value a, Value b)
 {
+#ifdef NAN_BOXING
+	if (IS_NUMBER(a) && IS_NUMBER(b))
+	{
+		return AS_NUMBER(a) == AS_NUMBER(b);
+	}
+	return a == b;
+#else
 	if (a.type != b.type)
 	{
 		return false;
@@ -19,6 +26,7 @@ bool valuesEqual(Value a, Value b)
 		case VAL_OBJ:		return AS_OBJ(a) == AS_OBJ(b);
 		default:			return false;	// Unreachable
 	}
+#endif	// NAN_BOXING
 }
 
 void initValueArray(ValueArray* array)
@@ -49,6 +57,26 @@ void writeValueArray(ValueArray* array, Value value)
 
 void printValue(Value value)
 {
+#ifdef NAN_BOXING
+	if (IS_BOOL(value))
+	{
+		printf(AS_BOOL(value) ? "true" : "false");
+	}
+	else if (IS_NIL(value))
+	{
+		printf("nil");
+	}
+	else if (IS_NUMBER(value))
+	{
+		printf("%g", AS_NUMBER(value));
+	}
+	else if (IS_OBJ(value))
+	{
+		printObject(value);
+	}
+
+#else	// NAN_BOXING
+
 	switch (value.type)
 	{
 		case VAL_BOOL:		printf(AS_BOOL(value) ? "true" : "false");	break;
@@ -56,4 +84,6 @@ void printValue(Value value)
 		case VAL_NUMBER:	printf("%g", AS_NUMBER(value));				break;
 		case VAL_OBJ:		printObject(value);							break;
 	}
+
+#endif	// NAN_BOXING
 }
